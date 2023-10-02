e456277795169e0e278c5359edbd3070f0146fce
AndreaMola05 <116425908+AndreaMola05@users.noreply.github.com>
2023-10-02 09:35:50 +0200

    fine

---
+++ es0/esA.php
@@ -0,0 +1,8 @@
+
+<?php
+echo "<b>Il mio primo script - PHP</b> </br>\n";
+echo "Il mio nome e' <b>Andrea Felice</b> <br> \n";
+echo "Il mio cognome e' <b>Mola</b> <br>\n";
+echo "Vivo a Triggiano <br>";
+echo "Provincia di BA <br>";
+?>
